e7282abd12e6cf5b5b83e133106f4e98e6457c75
Josh Gonzales <joshgonzales@ou.edu>
2019-10-16 14:37:18 -0500

    Modified tests

--- StudentTest.java
+++ StudentTest.java
@@ -47,8 +47,8 @@ public class StudentTest {
 			result2 = true;
 		assertTrue(result2);
 		
-		Student studentX = new Student("x", "Y", 5);
-		actual = student.compareTo(studentX);
+		Student studentSame = new Student("x", "Y", 5);
+		actual = student.compareTo(studentSame);
 		int expected = 0;
 		assertEquals(expected, actual);
 
